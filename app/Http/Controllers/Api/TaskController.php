a317e2661064c22ae54c3a8b073b89b8469ef564
mshoubasi <mr8wolf@gmail.com>
2022-06-27 13:25:42 +0300

    first commit

---
+++ app/Http/Controllers/Api/TaskController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreTaskRequest;
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $task = Task::all();
+        return response()->json([
+            'status' => true,
+            'tasks' => $task
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreTaskRequest $request)
+    {
+        $task = Task::create($request->all());
+
+        return response()->json([
+            'status' => true,
+            'tasks' => $task
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Task $task)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Task $task)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function update(StoreTaskRequest $request, Task $task)
+    {
+        $task->Update($request->all());
+
+        return response()->json([
+            'status' => true,
+            'message' => 'task updated',
+            'tasks' => $task
+        ]);
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Task  $task
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Task $task)
+    {
+        $task->delete();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'task deleted'
+        ]);
+
+
+    }
+}
